c6ee74d9a1a63c1d4b00317526eef0def178ab64
SuprunV <98488041+SuprunV@users.noreply.github.com>
2022-05-25 16:20:12 +0300

    added

---
+++ Project_1/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project_1.Infra;
+
+namespace Project_1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder b)
+        {
+            base.OnModelCreating(b);
+            initializeTables(b);
+        }
+        private static void initializeTables(ModelBuilder b)
+        {
+            UniversityDb.InitializeTables(b);
+        }
+
+    }
+}
